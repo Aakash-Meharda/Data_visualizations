a932edeccc369d67e8730f4c9f0561186202b786
Aakash <82360703+Aakash1521@users.noreply.github.com>
2024-06-07 23:00:16 +0530

    oo_plot_by_index

---
+++ 05_matplotlib_oo_plot_by_index.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+x = np.linspace(1,5,11)
+y=x**2
+
+fig,axes = plt.subplots(nrows=1,ncols=2)
+for current_ax in axes:
+    current_ax.plot(x,y)
+plt.show()
