a73fba5e0d55c679a5293182b9a5ca14453045cb
Aakash-Meharda <pv15official1@gmail.com>
2024-06-09 23:04:00 +0530

    Create 07_matplylib_oo_plot_by_index_size.py

---
+++ 07_matplylib_oo_plot_by_index_size.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+x = np.linspace(1,5,11)
+y=x**2
+
+fig,axes = plt.subplots(nrows=1,ncols=3,figsize=(10,2))
+
+
+axes[0].plot(x,y)
+axes[1].plot(y,x)
+axes[2].plot(x,y)
+
+plt.tight_layout()
+plt.show()
